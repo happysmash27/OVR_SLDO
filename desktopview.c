6e88190919c6fbec374aa31cdb01b10ab7e1038b
happysmash27 <happysmash27@vmail.me>
2021-02-06 10:46:31 -0800

    Removed init and deinit functions, as initialising in a function caused a segmentation fault

--- desktopview.c
+++ desktopview.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <xcb/xcb.h>
 
-int init(xcb_connection_t *xcb_connection, int *screen_number);
-int deinit(xcb_connection_t *xcb_connection);
-
 int print_screen_info(xcb_connection_t *xcb_connection, int *screen_number);
 
 uint8_t *grab_X11_drawable(xcb_drawable_t drawable) {
@@ -14,32 +11,13 @@ uint8_t *grab_X11_drawable(xcb_drawable_t drawable) {
 int main (int argc, char **argv){
   int *screen_number;
   xcb_connection_t *xcb_connection = xcb_connect(NULL, screen_number);
-  //if (init(xcb_connection, screen_number)){
-  //  printf("Initialisation failure!");
-  //  exit(EXIT_FAILURE);
-  //}
 
   if (print_screen_info(xcb_connection, screen_number)){
     exit(EXIT_FAILURE);
   }
 
-  if (deinit(xcb_connection)){
-    printf("Deinitialisation failure!");
-    exit(EXIT_FAILURE);
-  }
-  
-  return EXIT_SUCCESS;
-}
-
-int init(xcb_connection_t *xcb_connection, int *screen_number){
-  xcb_connection = xcb_connect(NULL, screen_number);
-
-  return EXIT_SUCCESS;
-}
-
-int deinit(xcb_connection_t *xcb_connection){
   xcb_disconnect(xcb_connection);
-
+  
   return EXIT_SUCCESS;
 }
 
