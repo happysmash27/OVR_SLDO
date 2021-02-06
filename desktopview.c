a5c796e61a441107f0ea7e2756df3bc03df0153f
happysmash27 <happysmash27@vmail.me>
2021-02-06 11:23:09 -0800

    Changed print_screen_info() to void; removed -Wno-uninitialized from makefile

--- desktopview.c
+++ desktopview.c
@@ -5,7 +5,7 @@
 void init(xcb_connection_t **xcb_connection, int *screen_number);
 void deinit(xcb_connection_t *xcb_connection);
 
-int print_screen_info(xcb_connection_t *xcb_connection, int *screen_number);
+void print_screen_info(xcb_connection_t *xcb_connection, int *screen_number);
 
 uint8_t *grab_X11_drawable(xcb_drawable_t drawable) {
   return 0;
@@ -16,16 +16,14 @@ int main (int argc, char **argv){
   xcb_connection_t *xcb_connection;
   init(&xcb_connection, &screen_number);
 
-  if (print_screen_info(xcb_connection, &screen_number)){
-    exit(EXIT_FAILURE);
-  }
+  print_screen_info(xcb_connection, &screen_number);
 
   deinit(xcb_connection);
   
   return EXIT_SUCCESS;
 }
 
-int print_screen_info(xcb_connection_t *xcb_connection, int *screen_number){
+void print_screen_info(xcb_connection_t *xcb_connection, int *screen_number){
   xcb_screen_t *xcb_screen;
   xcb_screen_iterator_t screen_iterator;
 
@@ -38,8 +36,6 @@ int print_screen_info(xcb_connection_t *xcb_connection, int *screen_number){
   printf("Height: %d\n", xcb_screen->height_in_pixels);
   printf("White Pixel: %d\n", xcb_screen->white_pixel);
   printf("Root depth: %d\n", xcb_screen->root_depth);
-
-  return EXIT_SUCCESS;
 }
 
 void init(xcb_connection_t **xcb_connection, int *screen_number){
