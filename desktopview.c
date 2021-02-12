1338a621cee8fb2b6d517f0c34eb16e1636f4c80
happysmash27 <happysmash27@vmail.me>
2021-02-11 18:56:30 -0800

    Restructured entire initialisation and context! Also, start of adding error messages.

--- desktopview.c
+++ desktopview.c
@@ -4,6 +4,7 @@
 #include <sys/shm.h>
 #include <xcb/xcb.h>
 #include <xcb/shm.h>
+#include <xcb/xcb_errors.h>
 
 #include <unistd.h>
 
@@ -14,14 +15,23 @@ typedef struct shm_framebuffer_t {
   uint32_t id;
   unsigned char *data;
 } shm_framebuffer_t;
-  
 
-void init(xcb_connection_t **xcb_connection, int *screen_number);
-void deinit(xcb_connection_t *xcb_connection);
+typedef struct xcb_context_t {
+  xcb_connection_t *connection;
+  int screen_number;
+  xcb_errors_context_t *errors_context;
+} xcb_context_t;
 
-void print_root_window_test(xcb_connection_t *xcb_connection);
+typedef struct ovr_sldo_context_t {
+  xcb_context_t *xcb_context;
+} ovr_sldo_context_t;
 
-void print_screen_info(xcb_connection_t *xcb_connection, int *screen_number);
+ovr_sldo_context_t *init();
+void deinit(ovr_sldo_context_t *ovr_sldo);
+
+void print_root_window_test(xcb_context_t *xcb_context);
+
+void print_screen_info(xcb_context_t *xcb_context);
 
 int write_image_ppm(uint8_t image_data);
 
@@ -30,24 +40,25 @@ uint8_t *grab_X11_drawable(xcb_drawable_t drawable) {
 }
 
 int main (int argc, char **argv){
-  int screen_number;
-  xcb_connection_t *xcb_connection;
-  init(&xcb_connection, &screen_number);
+  ovr_sldo_context_t *ovr_sldo = init();
 
-  print_root_window_test(xcb_connection);
+  //Just printing the screen info as a test to make sure my Great Variable Shift works
+  print_screen_info(ovr_sldo->xcb_context);
+  //What I actually want to do -- try to get an image of my screen, as a test
+  print_root_window_test(ovr_sldo->xcb_context);
 
-  deinit(xcb_connection);
+  deinit(ovr_sldo);
   
   return EXIT_SUCCESS;
 }
 
-void print_root_window_test(xcb_connection_t *xcb_connection){
+void print_root_window_test(xcb_context_t *xcb_context){
   //So, I'm not quite sure how this thing works, because documentation is terrible.
   //For this reason, I am creating a simple algorithm outside of the structure I have tried to set up, which will serve as a test for whether I understand XCB SHM correctly
   //My aim is to grab an SHM image, and print it as a PPM
 
   //Set up screen data. We will need to know how big our buffer needs to be
-  xcb_screen_iterator_t screen_iterator = xcb_setup_roots_iterator(xcb_get_setup(xcb_connection));
+  xcb_screen_iterator_t screen_iterator = xcb_setup_roots_iterator(xcb_get_setup(xcb_context->connection));
   xcb_screen_t *xcb_screen = screen_iterator.data;
 
   //We need a framebuffer for our data... I think. I'm not sure if I am supposed to get a shared memory buffer from XCB, or if I'm supposed to make it myself, but I'll try the latter for now as it looks the most promising.
@@ -67,8 +78,8 @@ void print_root_window_test(xcb_connection_t *xcb_connection){
   framebuffer1->id = shmget(IPC_PRIVATE, framebuffer1->size, IPC_CREAT | 0666);
   
   //So, uh... I think what I am supposed to do is create an shm segment by requesting an ID from XCB, then attach that that shm segment to my shared memory using xcb_shm_attach? I think this is what ffmpeg does. 
-  xcb_shm_seg_t shm_segment = xcb_generate_id(xcb_connection); //This thing has no man page :( . Why is documentation for this library so horrible?
-  xcb_shm_attach(xcb_connection, shm_segment, framebuffer1->id, 0);
+  xcb_shm_seg_t shm_segment = xcb_generate_id(xcb_context->connection); //This thing has no man page :( . Why is documentation for this library so horrible?
+  xcb_shm_attach(xcb_context->connection, shm_segment, framebuffer1->id, 0);
 
   //So, our SHM is attached... hopefully. If the program hasn't crashed by now, we will use xcb_shm_get_image, which I think tells it to fill the shared memory buffer with the image? I don't see any way to get the image from the actual reply. Then, we will attach to it. 
 
@@ -79,26 +90,26 @@ void print_root_window_test(xcb_connection_t *xcb_connection){
 
   //Now we fill them.
   //I'm not sure what the differences are between the xcb_image_format_ts are, as yet again, I can find nothing online! Such horrible documentation! Anyways, I will do what ffmpeg does, using XCB_IMAGE_FORMAT_Z_PIXMAP, then see what happens if I use XCB_IMAGE_FORMAT_XY_PIXMAP instead, since that also sounds like something I would want
-  image_request = xcb_shm_get_image(xcb_connection, xcb_screen->root, 0, 0, framebuffer1->width, framebuffer1->height, ~0, XCB_IMAGE_FORMAT_Z_PIXMAP , shm_segment, 0);
+  image_request = xcb_shm_get_image(xcb_context->connection, xcb_screen->root, 0, 0, framebuffer1->width, framebuffer1->height, ~0, XCB_IMAGE_FORMAT_Z_PIXMAP , shm_segment, 0);
 
   //And now we immediately request it back. I think this causes it to wait until it has been gathered?
   //Try not to do error handling now, but if the null causes problems, I might have to add it to this early testing function
-  image_reply = xcb_shm_get_image_reply(xcb_connection, image_request, &e);
+  image_reply = xcb_shm_get_image_reply(xcb_context->connection, image_request, &e);
 
   if (e){
     fprintf(stderr, "Error getting image reply!\n"
 	    "\n"
-	    "Response type: %u\n"
-	    "Error code: %u\n"
-	    "Sequence number: %u\n"
-	    "Resource ID: %u\n"
-	    "Minor opcode: %u\n"
-	    "Major opcode: %u\n"
+	    "Response type %u\n"
+	    "Error code %u\n"
+	    "Sequence number %u\n"
+	    "Resource ID %u\n"
+	    "Minor opcode %u\n"
+	    "Major opcode %u\n"
 	    "\n", e->response_type, e->error_code, e->sequence, e->resource_id, e->minor_code, e->major_code);
   }
 
   //Flush things, just to be sure
-  xcb_flush(xcb_connection);
+  xcb_flush(xcb_context->connection);
 
   //Because ffmpeg did it, and something is stopping the capture from working
   free(image_reply);
@@ -121,7 +132,7 @@ void print_root_window_test(xcb_connection_t *xcb_connection){
   }
 
   //We are done. Close everything.
-  xcb_flush(xcb_connection);
+  xcb_flush(xcb_context->connection);
   //free(framebuffer1->data);
   shmdt(framebuffer1->data);
   
@@ -129,14 +140,14 @@ void print_root_window_test(xcb_connection_t *xcb_connection){
   
 }
 
-void print_screen_info(xcb_connection_t *xcb_connection, int *screen_number){
+void print_screen_info(xcb_context_t *xcb_context){
   xcb_screen_t *xcb_screen;
   xcb_screen_iterator_t screen_iterator;
 
-  printf("Screen number: %d\n", *screen_number);
+  printf("Screen number: %d\n", xcb_context->screen_number);
 
   //XCB example does some weird thing to get to screen number 0. Look into this to improve this later, but as this likely isn't very important, I will leave this as is for now. 
-  screen_iterator = xcb_setup_roots_iterator(xcb_get_setup(xcb_connection));
+  screen_iterator = xcb_setup_roots_iterator(xcb_get_setup(xcb_context->connection));
   xcb_screen = screen_iterator.data;
 
   printf("Width: %d\n", xcb_screen->width_in_pixels);
@@ -145,10 +156,16 @@ void print_screen_info(xcb_connection_t *xcb_connection, int *screen_number){
   printf("Root depth: %d\n", xcb_screen->root_depth);
 }
 
-void init(xcb_connection_t **xcb_connection, int *screen_number){
-  *xcb_connection = xcb_connect(NULL, screen_number);
+ovr_sldo_context_t *init(){
+  ovr_sldo_context_t *ovr_sldo = malloc(sizeof (ovr_sldo_context_t));
+  
+  ovr_sldo->xcb_context = malloc(sizeof (xcb_context_t));
+  ovr_sldo->xcb_context->connection = xcb_connect(NULL, &ovr_sldo->xcb_context->screen_number);
+  xcb_errors_context_new(ovr_sldo->xcb_context->connection, &(ovr_sldo->xcb_context->errors_context));
+
+  return ovr_sldo;
 }
 
-void deinit(xcb_connection_t *xcb_connection){
-  xcb_disconnect(xcb_connection);
+void deinit(ovr_sldo_context_t *ovr_sldo){
+  xcb_disconnect(ovr_sldo->xcb_context->connection);
 }
